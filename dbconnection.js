c4315ab3832ce8a3f9fb2852791524d44cb20218
Vasco Marques <vasco_marques_ei@hotmail.com>
2017-07-04 17:44:01 +0100

    sada
    asd

--- dbconnection.js
+++ dbconnection.js
@@ -1,9 +1,10 @@
 var mysql=require('mysql');
  var connection=mysql.createPool({
 
+//command get password mysql sudo grep 'temporary password' /var/log/mysqld.log
 host:'localhost',
  user:'root',
- password:'root',
+ password:'Si<_iO)q8ekj',
  database:'app'
 
 });
